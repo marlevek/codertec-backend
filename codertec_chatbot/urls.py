a57b7152fac68792773f4d2420d538865df71317
marlevek <marcelo_levek@hotmail.com>
2025-10-26 10:54:54 -0300

    definidas as rotas urls

--- codertec_chatbot/urls.py
+++ codertec_chatbot/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings 
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,3 +9,5 @@ urlpatterns = [
     path('api/', include('chatbot.urls')),  # 🔹 Inclui as rotas do app chatbot
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
